a093dca2dc346f32acb32e7720a0022912e66cb4
Jason Adams <adams.jason.029@gmail.com>
2022-01-10 10:52:01 -0500

    Add project files.

---
+++ BoardGameInventory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoardGameInventory.Startup))]
+namespace BoardGameInventory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
